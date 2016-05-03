52876f37524f76e57a686d64bd80491beb53a125
João Guilherme <j@guiatech.com.br>
2016-05-03 12:43:11 -0300

    Add share button and download button :art:

--- static/js/1.0/assets/common.js
+++ static/js/1.0/assets/common.js
@@ -7,19 +7,20 @@ temwhats.config = {
     
     init: function(){
         var container_body = document.getElementsByTagName('body')[0],
+        _obj_download_gplay = document.getElementsByClassName('download-google-play')[0],
         _container_whats_on = document.getElementsByClassName('content-whats-on')[0],
-        _twitter_whats_on = document.getElementsByClassName('twitter-whats-on')[0],
-        _twitter_whats_off = document.getElementsByClassName('twitter-whats-off')[0],
         _container_whats_off = document.getElementsByClassName('content-whats-off')[0];
         
         if (this.status_whats.toLowerCase() === 'on') {
             container_body.className = container_body.className + ' whats-on';
             _container_whats_on.className = _container_whats_on.className + ' showing';
-            _twitter_whats_on.className = _twitter_whats_on.className + ' showing';
         } else if (this.status_whats.toLowerCase() === 'off') {
             container_body.className = container_body.className + ' whats-off';
             _container_whats_off.className = _container_whats_off.className + ' showing';
-            _twitter_whats_off.className = _twitter_whats_off.className + ' showing';
+        }
+        
+        if( window.navigator.userAgent.toLowerCase() !== 'app' ){
+            _obj_download_gplay.className = _obj_download_gplay.className + ' showing';
         }
         
         console.log('Current status:', this.status_whats);
